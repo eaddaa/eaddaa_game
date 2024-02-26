e5b45f237715c12222da76edd645e2bd6f7a8435
root <root@vmi1531223.contaboserver.net>
2024-02-27 00:27:55 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'tea_weather_app==0.1.0',
     ],
+    url='https://github.com/eaddaa/eaddaa-game',  # Buraya kendi Git deposunuzun URL'sini ekleyin
     entry_points={
         'console_scripts': [
             'eaddaa-game=eaddaa_game.game_logic:play_game',
