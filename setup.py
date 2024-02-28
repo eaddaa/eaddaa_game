bc16f51068dc5c60c981fdcfc46f835d72c41745
root <root@vmi1531223.contaboserver.net>
2024-02-28 18:29:40 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eaddaa-game',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'tea_weather_app==0.1.0',
