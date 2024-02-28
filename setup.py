b8dd5c31f8d60ce3015788d1a952af7bbab00c87
root <root@vmi1531223.contaboserver.net>
2024-02-28 18:46:45 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='eaddaa-game',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         'tea_weather_app==0.1.0',
     ],
-    url='https://github.com/eaddaa/eaddaa_game',
     entry_points={
         'console_scripts': [
             'eaddaa-game=eaddaa_game.game_logic:play_game',
@@ -17,6 +15,9 @@ setup(
     author='eaddaa',
     author_email='eaddaa@gmail.com',
     description='eaddaa-game_project',
+    bugtrack_url='https://github.com/eaddaa/eaddaa_game',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
